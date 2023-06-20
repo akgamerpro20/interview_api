be29996f14bcce9b440ef1fb2d2223a6ebcac5a6
aungkhant2000 <akgamerpro20@gmail.com>
2023-06-20 21:45:25 +0630

    test auto deploy

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <h2>Hello</h2>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
